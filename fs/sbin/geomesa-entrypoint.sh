f048c6148ab6e2a0b4fbb883545d711cff014373
Eugene Cheipesh <echeipesh@gmail.com>
2016-08-04 16:25:12 -0400

    copy geomesa lib to HDFS

--- fs/sbin/geomesa-entrypoint.sh
+++ fs/sbin/geomesa-entrypoint.sh
@@ -7,12 +7,17 @@ source /sbin/accumulo-lib.sh
 # The first argument determines this container's role in the accumulo cluster
 ROLE=${1:-}
 USER=${USER:-root}
+HDFS_ROOT=${HDFS_ROOT:-}
 ACCUMULO_USER=${ACCUMULO_USER:-root}
 
-enable_iterators(){
+enable_iterators()
+  ROOT=${HADOOP_MASTER_ADDRESS:$(xmllint --xpath "//property[name='fs.defaultFS']/value/text()"  /etc/hadoop/conf/core-site.xml)}
+  HDFS_LIB_DIR=hdfs://$ROOT/accumulo-classpath/geomesa
+  runuser -p -u $USER -- hdfs dfs -mkdir -p $HDFS_LIB_DIR
+  runuser -p -u $USER -- hdfs dfs -copyFromLocal /opt/geomesa/accumulo/* $HDFS_LIB_DIR
   accumulo shell -u ${ACCUMULO_USER} -p ${ACCUMULO_PASSWORD} <<-EOF
     createnamespace geomesa
-    config -s general.vfs.context.classpath.geomesa=file:///opt/geomesa/accumulo/geomesa-accumulo-distributed-runtime-${GEOMESA_VERSION}.jar
+    config -s general.vfs.context.classpath.geomesa=${HDFS_LIB_DIR}/[^.].*.jar
     config -ns geomesa -s table.classpath.context=geomesa
 EOF
 }
@@ -22,4 +27,3 @@ if [[ $ROLE = "master" ]]; then
 fi
 
 /sbin/entrypoint.sh "$@"
-
